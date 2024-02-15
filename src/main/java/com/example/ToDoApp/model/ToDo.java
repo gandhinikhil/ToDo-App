ddf34ef67bebb66657b7c294d226dd028424f99e
Nikhil <gwl.nikhil@gmail.com>
2024-02-16 02:14:24 +0530

    Entity class

--- src/main/java/com/example/ToDoApp/model/ToDo.java
+++ src/main/java/com/example/ToDoApp/model/ToDo.java
@@ -1,5 +1,67 @@
 package com.example.ToDoApp.model;
 
-public class ToDo {
+import java.util.Date;
 
+import org.hibernate.annotations.Columns;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "todo")
+public class ToDo {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Column
+	private String title;
+	
+	@Column
+	private Date date;
+	
+	@Column
+	private String status;
+	
+	public ToDo() {
+		
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
 }
